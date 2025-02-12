c2125265fa58c46be91c2eae87c7c72a87094e40
fajar abdillah <fajardillah25@gmail.com>
2025-02-12 23:08:33 +0700

    Upload Consumer

---
+++ springboot-reactive-kafka-demo/src/main/java/dev/codejar/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,27 @@
+package dev.codejar.kafka.consumer;
+
+
+import dev.codejar.kafka.payload.Student;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+
+
+//    @KafkaListener(topics = "fajar", groupId = "myCodejar")
+    public void consumedMessage(String message){
+        log.info("Consuming the message from fajar topic :: {}", message);
+    }
+
+    @KafkaListener(topics = "fajar", groupId = "myCodejar")
+    public void consumeJsonMessage(Student student){
+        log.info("Consuming the message from fajar topic:: {}", student.toString());
+    }
+
+
+}
